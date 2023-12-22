badfe2299e0940df95f714348531e5649de4a5b5
nikhilpersonal <153951567+nikhilpersonal@users.noreply.github.com>
2023-12-21 21:02:26 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -228,7 +228,7 @@ def main():
         try:
             s = total_won/t
             s = str(s)
-            st.metric(label = "units up/down", value = f" :green[{s}] + u ")
+            st.metric(label = "units up/down", value = f" hi :green[{s}] u ")
         except:
             s= 0
             st.metric(label = "units up/down", value = str(s) + " u")
