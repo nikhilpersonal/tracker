b0b0be1bf86397e7cca969cd1b13fd932402d675
nikhilpersonal <153951567+nikhilpersonal@users.noreply.github.com>
2023-12-27 01:10:46 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -263,7 +263,7 @@ def main():
     with col6: 
         try:
             s = total_won/t
-            s = str(s)
+            s = round(s,2)
             st.metric(label = "units up/down", value = (f" {s} u "))
         except:
             s= 0
